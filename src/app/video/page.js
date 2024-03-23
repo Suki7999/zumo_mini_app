b5301843b2d76ed4f62d638a295cbde2618f665c
MTG - PHH - Sukhbaatar - 91650 <sukhbaatar.g@golomtbank.com>
2024-03-23 18:00:23 +0800

    uptodate

--- src/app/video/page.js
+++ src/app/video/page.js
@@ -1,7 +1,29 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import Layout from '/src/components/Layouts'
+import { usePathname } from 'next/navigation';
+import { CldVideoPlayer } from 'next-cloudinary';
+import { axiosInstance } from '../axios/axiosInstance';
 
 export default function page() {
+  const pathname  = usePathname();
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      let response = await axiosInstance.get('/api/sales/v1/rest/videos/');
+      setVideos(response.data.result);
+    }
+    fetchVideos();
+  }, [])
   return (
-    <div>page</div>
+    <Layout>
+      <p className='text-2xl text-white'>{pathname}</p>
+      {
+        videos && videos.map((v) => (
+          <CldVideoPlayer width="400" height="260" src={`${v.link_to_connect}`}/>
+        ))
+      }
+    </Layout>
   )
 }
