d3f5881d6c9a80387d90faa8c4fd9ce99ca6f24a
user <user@users-MacBook-Pro.local>
2024-03-23 20:50:32 +0800

    update video

--- src/app/video/page.js
+++ src/app/video/page.js
@@ -18,18 +18,18 @@ export default function page() {
   }, [])
   return (
     <Layout layoutType="video">
-      <div className='bg-violet-500 flex items-center justify-items-start w-full h-[68px] rounded-b-md text-white my-4 cursor-pointer'>
+      <div className='bg-violet-500 flex items-center justify-items-start w-full h-[68px] rounded-b-md text-white my-6 cursor-pointer'>
           <Image src={pathname.includes('video') ? `/images/logo/1.png` : ''} width={46} height={39} alt=''/>
           <p className='text-[30px] ml-28 font-bold text-center'>100</p>
       </div>
       {
         videos && videos.map((v, i) => (
-          <div className='bg-inherit px-2 static'>
-            <div className='bg-violet-500 rounded-lg w-[97px] h-[37.3px] absolute left-[72%] flex justify-between items-center p-2'>
+          <div className='bg-inherit px-1 static'>
+            <div className='bg-violet-500 rounded-lg w-[97px] h-[37.3px] absolute left-[72%] flex justify-between items-center p-2 mt-2'>
               <p className='text-white'>{v.loyalty_amount}</p>
               <img className='w-[56px] h-[37.3px]' src="/images/logo/7.png" alt="money" />
             </div>
-            <iframe key={i} className='rounded-t-lg h-[219px] w-full mt-[37.3px] static' src={`https://www.youtube.com/embed/${v.video_link}?controls=0`} allowFullScreen />
+            <iframe key={i} className='rounded-t-lg h-[219px] w-full static' src={`https://www.youtube.com/embed/${v.video_link}?controls=0`} allowFullScreen />
           </div>
         ))
       }
