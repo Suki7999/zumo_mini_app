343def5cf3f2817efc76c93707a5e20ed4715d9e
MTG - PHH - Sukhbaatar - 91650 <sukhbaatar.g@golomtbank.com>
2024-03-24 14:03:37 +0800

    updateto

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -1,25 +1,47 @@
 'use client'
+import React, { useState } from 'react';
 import SideBar from '/src/components/Sidebar';
 import Header from '/src/components/Header';
 import BreadCrumbs from '/src/app/components/Breadcrumbs';
 
-import { useState } from 'react';
+const Layout = ({ children, layoutType }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
 
-const Layout = ({ children, layoutType}) => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const toggleSideBar = () => {
+    setIsSideBarOpen(!isSideBarOpen);
+  };
+
   return (
     <div className="flex h-screen">
-      <div className="w-1/5 h-[100%] text-white hidden md:block"> {/* Hide on smaller screens */}
-        <SideBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+
+      <div className="w-1/5 h-[100%] text-white hidden md:block">
+        <SideBar isOpen={isSideBarOpen}/>
       </div>
+
       <div className="flex flex-col flex-1">
-        {
-          layoutType ? <BreadCrumbs crumb={layoutType}/> : <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
-        }
+
+        {layoutType ? <BreadCrumbs crumb={layoutType}/> : <Header toggleSideBar={toggleSideBar}/>}
+
         <main className="p-2">
           {children}
         </main>
       </div>
+      
+      {isSideBarOpen && (
+        <div className="md:hidden fixed inset-0 bg-gray-800 bg-opacity-75 z-20">
+          <div className="absolute top-0 right-0 mt-4 mr-4">
+            <button 
+              className="text-white"
+              onClick={toggleSideBar}
+            >
+              Close
+            </button>
+          </div>
+          <div className="flex justify-end pt-16 px-4">
+            <SideBar isOpen={isSideBarOpen}/>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
